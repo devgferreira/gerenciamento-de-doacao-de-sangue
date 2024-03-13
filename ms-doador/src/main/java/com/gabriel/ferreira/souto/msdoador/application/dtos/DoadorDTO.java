d91cac5085e35dea292cdfd5c4d826e5509e6415
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-13 18:12:00 -0300

    feat: adicionando os atributos no DoadorDTO - DoadorDTO

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/dtos/DoadorDTO.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/dtos/DoadorDTO.java
@@ -1,4 +1,20 @@
 package com.gabriel.ferreira.souto.msdoador.application.dtos;
 
+import com.gabriel.ferreira.souto.msdoador.domain.model.endereco.Endereco;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DoadorDTO {
+    private Integer Id;
+    private String Nome;
+    private String Email;
+    private Date Aniversario;
+    private String Genero;
+    private Integer Peso;
+    private String TipoSanguineo;
 }
