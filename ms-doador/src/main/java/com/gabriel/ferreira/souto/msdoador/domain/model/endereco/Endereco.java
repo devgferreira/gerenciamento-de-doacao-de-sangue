01c070b4ba7809f50374c74db4e9dad313468b04
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-13 14:22:09 -0300

    feat: atribuindo os atributos na entidade Endereco - Endereco

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/model/endereco/Endereco.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/model/endereco/Endereco.java
@@ -1,4 +1,22 @@
 package com.gabriel.ferreira.souto.msdoador.domain.model.endereco;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String Bairro;
+    private String Cidade;
+    private String Estado;
+    private String Cep;
 }
