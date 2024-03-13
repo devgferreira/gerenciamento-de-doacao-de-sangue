f6f1cc8b9bf611225e0fb2c2c7e98ddb544a4809
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-13 14:23:37 -0300

    feat: atribuindo os atributos na entidade Doador - Doador

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/model/doador/Doador.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/model/doador/Doador.java
@@ -1,6 +1,31 @@
 package com.gabriel.ferreira.souto.msdoador.domain.model.doador;
 
+import com.gabriel.ferreira.souto.msdoador.domain.model.endereco.Endereco;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Doador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String Nome;
+    private String Email;
+    private Date Aniversario;
+    private String Genero;
+    private Integer Peso;
+    private String TipoSanguineo;
+    private Endereco Endereco;
+
 }
