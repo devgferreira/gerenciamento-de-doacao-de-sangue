bc270138408a99ed015956dfde1ae0695323bbbf
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 12:50:43 -0300

    feat: implementando o método: criarDoador - DoadorService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorService.java
@@ -1,8 +1,12 @@
 package com.gabriel.ferreira.souto.msdoador.application.service;
 
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorDTO;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorRequestDTO;
 import com.gabriel.ferreira.souto.msdoador.application.interfaces.IDoadorService;
+import com.gabriel.ferreira.souto.msdoador.application.interfaces.IEnderecoService;
 import com.gabriel.ferreira.souto.msdoador.domain.interfaces.IDoadorRepository;
+import com.gabriel.ferreira.souto.msdoador.domain.model.doador.Doador;
+import com.gabriel.ferreira.souto.msdoador.domain.model.doador.request.DoadorRequest;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +14,20 @@ import org.springframework.stereotype.Service;
 public class DoadorService implements IDoadorService {
 
     private final ModelMapper _modelMapper;
+    private final IEnderecoService _enderecoService;
     private final IDoadorRepository _doadorRepository;
 
-    public DoadorService(ModelMapper modelMapper, IDoadorRepository doadorRepository) {
+    public DoadorService(ModelMapper modelMapper, IEnderecoService enderecoService, IDoadorRepository doadorRepository) {
         _modelMapper = modelMapper;
+        _enderecoService = enderecoService;
         _doadorRepository = doadorRepository;
     }
 
     @Override
-    public DoadorDTO criarDoador(DoadorDTO doadorDTO) {
-        return null;
+    public DoadorDTO criarDoador(DoadorRequestDTO doadorRequestDTO) {
+        Doador doador = _modelMapper.map(doadorRequestDTO, Doador.class);
+        _enderecoService.criarEndereco(doadorRequestDTO.getEnderecoDTO());
+        return _modelMapper.map(_doadorRepository.save(doador), DoadorDTO.class);
     }
 
     @Override
