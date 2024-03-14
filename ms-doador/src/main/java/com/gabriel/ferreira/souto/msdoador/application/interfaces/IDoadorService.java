9eabcbf2e4347a380c924072b2ac34ddf1d5125f
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 19:20:12 -0300

    feat: Mudando o retorno do método buscarDoadorComId para DoadorResponseDTO - IDoadorService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IDoadorService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IDoadorService.java
@@ -2,11 +2,12 @@ package com.gabriel.ferreira.souto.msdoador.application.interfaces;
 
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorDTO;
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorRequestDTO;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorResponseDTO;
 import com.gabriel.ferreira.souto.msdoador.domain.model.doador.request.DoadorRequest;
 
 public interface IDoadorService {
     DoadorDTO criarDoador(DoadorRequestDTO doadorRequestDTO);
     DoadorDTO atualizarDoador(DoadorRequestDTO doadorRequestDTO, Integer doadorId);
-    DoadorRequestDTO buscarDoadorComId(Integer doadorId);
+    DoadorResponseDTO buscarDoadorComId(Integer doadorId);
     void deletarDoadorComId(Integer doadorId);
 }
