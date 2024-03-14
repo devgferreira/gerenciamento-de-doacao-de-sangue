2131e8ec849753f2b2059f6af9e9ad3cfd31ca37
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 12:47:45 -0300

    feat: criando o DTO: DoadorRequestDTO - DoadorRequestDTO

---
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/dtos/DoadorRequestDTO.java
@@ -0,0 +1,21 @@
+package com.gabriel.ferreira.souto.msdoador.application.dtos;
+
+import com.gabriel.ferreira.souto.msdoador.domain.model.endereco.Endereco;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoadorRequestDTO {
+    private String Nome;
+    private String Email;
+    private Date Aniversario;
+    private String Genero;
+    private Integer Peso;
+    private String TipoSanguineo;
+    private EnderecoDTO enderecoDTO;
+}
