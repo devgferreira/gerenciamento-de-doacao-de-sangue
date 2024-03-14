36ca481d9d76ec0f1ee7aae8f77b17221728a3e7
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 12:41:03 -0300

    feat: implementando o interface: IDoadorService - DoadorService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorService.java
@@ -1,11 +1,13 @@
 package com.gabriel.ferreira.souto.msdoador.application.service;
 
+import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorDTO;
+import com.gabriel.ferreira.souto.msdoador.application.interfaces.IDoadorService;
 import com.gabriel.ferreira.souto.msdoador.domain.interfaces.IDoadorRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DoadorService {
+public class DoadorService implements IDoadorService {
 
     private final ModelMapper _modelMapper;
     private final IDoadorRepository _doadorRepository;
@@ -13,5 +15,25 @@ public class DoadorService {
     public DoadorService(ModelMapper modelMapper, IDoadorRepository doadorRepository) {
         _modelMapper = modelMapper;
         _doadorRepository = doadorRepository;
+    }
+
+    @Override
+    public DoadorDTO criarDoador(DoadorDTO doadorDTO) {
+        return null;
+    }
+
+    @Override
+    public DoadorDTO atualizarDoador(DoadorDTO doadorDTO, Integer doadorId) {
+        return null;
+    }
+
+    @Override
+    public DoadorDTO buscarDoadorComId(Integer doadorId) {
+        return null;
+    }
+
+    @Override
+    public void deletarDoadorComId(Integer doadorId) {
+
     }
 }
