25c7e266bc1e17bd9eab99cfe1e470bcd453d642
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 13:12:47 -0300

    feat: adicionando mais um parametro no bsucarEnderecoComDoadorId - IEnderecoService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IEnderecoService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IEnderecoService.java
@@ -1,8 +1,9 @@
 package com.gabriel.ferreira.souto.msdoador.application.interfaces;
 
 import com.gabriel.ferreira.souto.msdoador.application.dtos.EnderecoDTO;
+import com.gabriel.ferreira.souto.msdoador.domain.model.endereco.Endereco;
 
 public interface IEnderecoService {
     EnderecoDTO criarEndereco(EnderecoDTO enderecoDTO);
-    EnderecoDTO buscarEndercoComDoadorId(Integer doadorId);
+    EnderecoDTO buscarEnderecoComDoadorId(EnderecoDTO enderecoDTO, Integer doadorId);
 }
