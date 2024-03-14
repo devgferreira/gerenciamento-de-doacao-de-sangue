d2546c4734766102e06612d16d0f76716e39c448
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 12:58:59 -0300

    feat: criando a query para a buscar de um endereco com o id do doador - IEnderecoRepository

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IEnderecoRepository.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IEnderecoRepository.java
@@ -4,6 +4,9 @@ import com.gabriel.ferreira.souto.msdoador.domain.model.endereco.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IEnderecoRepository extends JpaRepository<Endereco, Integer> {
+    Optional<Endereco> findEnderecoByDoadorId(Integer doadorId);
 }
