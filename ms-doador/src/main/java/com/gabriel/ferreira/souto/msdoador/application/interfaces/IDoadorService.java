f1229076ac6cf511618d1c24fbaa4f2174c88f14
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 12:48:32 -0300

    feat: mudando o tipo do parametro do criaDoador para DoadorRequestDTO - IDoadorService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IDoadorService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IDoadorService.java
@@ -1,9 +1,11 @@
 package com.gabriel.ferreira.souto.msdoador.application.interfaces;
 
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorDTO;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorRequestDTO;
+import com.gabriel.ferreira.souto.msdoador.domain.model.doador.request.DoadorRequest;
 
 public interface IDoadorService {
-    DoadorDTO criarDoador(DoadorDTO doadorDTO);
+    DoadorDTO criarDoador(DoadorRequestDTO doadorRequestDTO);
     DoadorDTO atualizarDoador(DoadorDTO doadorDTO, Integer doadorId);
     DoadorDTO buscarDoadorComId(Integer doadorId);
     void deletarDoadorComId(Integer doadorId);
