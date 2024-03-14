a9b058b75ef21acb382cca0e92acb1be00842ad3
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 19:14:37 -0300

    feat: criando o construtor para interface IDoadorService - DoadorController

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/apresentation/DoadorController.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/apresentation/DoadorController.java
@@ -1,4 +1,17 @@
 package com.gabriel.ferreira.souto.msdoador.apresentation;
 
+import com.gabriel.ferreira.souto.msdoador.application.interfaces.IDoadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/doadores")
 public class DoadorController {
+    private final IDoadorService _doadorService;
+
+    @Autowired
+    public DoadorController(IDoadorService doadorService) {
+        _doadorService = doadorService;
+    }
 }
