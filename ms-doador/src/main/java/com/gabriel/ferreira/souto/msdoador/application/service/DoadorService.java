3eef9c8ea64ee05759ed8352f175178a36c9677b
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 12:40:04 -0300

    feat: criando o construtor para o modelMapper e IDoadorRepository - DoadorService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorService.java
@@ -1,4 +1,17 @@
 package com.gabriel.ferreira.souto.msdoador.application.service;
 
+import com.gabriel.ferreira.souto.msdoador.domain.interfaces.IDoadorRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DoadorService {
+
+    private final ModelMapper _modelMapper;
+    private final IDoadorRepository _doadorRepository;
+
+    public DoadorService(ModelMapper modelMapper, IDoadorRepository doadorRepository) {
+        _modelMapper = modelMapper;
+        _doadorRepository = doadorRepository;
+    }
 }
