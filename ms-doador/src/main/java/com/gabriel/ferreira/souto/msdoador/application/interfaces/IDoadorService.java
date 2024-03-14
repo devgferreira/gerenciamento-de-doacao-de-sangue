59db6f35eef0f38360437e01185fb78e0d370a5d
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 19:53:29 -0300

    feat: adicionando mais um parametro no criarEndereco - IEnderecoService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IDoadorService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IDoadorService.java
@@ -3,7 +3,6 @@ package com.gabriel.ferreira.souto.msdoador.application.interfaces;
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorDTO;
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorRequestDTO;
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorResponseDTO;
-import com.gabriel.ferreira.souto.msdoador.domain.model.doador.request.DoadorRequest;
 
 public interface IDoadorService {
     DoadorDTO criarDoador(DoadorRequestDTO doadorRequestDTO);
