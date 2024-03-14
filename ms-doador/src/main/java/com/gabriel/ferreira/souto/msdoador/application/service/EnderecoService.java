d7a94191bcf173c25db39166bbf0c27f04ca2d5a
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 11:22:44 -0300

    feat: criando o construtor para ModelMapper e IEnderecoRepository - EnderecoService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/EnderecoService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/EnderecoService.java
@@ -1,4 +1,18 @@
 package com.gabriel.ferreira.souto.msdoador.application.service;
 
+import com.gabriel.ferreira.souto.msdoador.domain.interfaces.IEnderecoRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EnderecoService {
+    private final ModelMapper _modelMapper;
+    private final IEnderecoRepository _enderecoRepository;
+
+    public EnderecoService(ModelMapper modelMapper, IEnderecoRepository enderecoRepository) {
+        _modelMapper = modelMapper;
+        _enderecoRepository = enderecoRepository;
+    }
 }
+
+
