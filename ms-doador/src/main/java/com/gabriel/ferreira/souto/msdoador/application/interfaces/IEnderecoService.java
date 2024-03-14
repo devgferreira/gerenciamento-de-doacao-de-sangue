3257a5fef40485dfcca67ed9d88af146922e0bc0
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 10:47:42 -0300

    feat: criando o contrato criarEndereco - IEnderecoService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IEnderecoService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IEnderecoService.java
@@ -3,5 +3,5 @@ package com.gabriel.ferreira.souto.msdoador.application.interfaces;
 import com.gabriel.ferreira.souto.msdoador.application.dtos.EnderecoDTO;
 
 public interface IEnderecoService {
-
+    EnderecoDTO criarEndereco(EnderecoDTO enderecoDTO);
 }
