81c10298db31b69f7dc383776aaaed8edaea0a1b
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 19:52:45 -0300

    feat: criando o método para buscar um Doador pelo seu Email - IDoadorRepository

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IDoadorRepository.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IDoadorRepository.java
@@ -4,6 +4,9 @@ import com.gabriel.ferreira.souto.msdoador.domain.model.doador.Doador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IDoadorRepository extends JpaRepository<Doador, Integer> {
+    Optional<Doador>findByEmail(String email);
 }
