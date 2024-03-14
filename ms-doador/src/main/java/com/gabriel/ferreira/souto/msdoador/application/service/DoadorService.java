f7143b12cb91823dd4212045c2d961702645a46c
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-14 15:07:43 -0300

    feat: implementando o método atualizarDoador - DoadorService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorService.java
@@ -2,13 +2,17 @@ package com.gabriel.ferreira.souto.msdoador.application.service;
 
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorDTO;
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorRequestDTO;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.EnderecoDTO;
 import com.gabriel.ferreira.souto.msdoador.application.interfaces.IDoadorService;
 import com.gabriel.ferreira.souto.msdoador.application.interfaces.IEnderecoService;
 import com.gabriel.ferreira.souto.msdoador.domain.interfaces.IDoadorRepository;
 import com.gabriel.ferreira.souto.msdoador.domain.model.doador.Doador;
 import com.gabriel.ferreira.souto.msdoador.domain.model.doador.request.DoadorRequest;
+import com.gabriel.ferreira.souto.msdoador.domain.model.endereco.Endereco;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DoadorService implements IDoadorService {
@@ -31,8 +35,25 @@ public class DoadorService implements IDoadorService {
     }
 
     @Override
-    public DoadorDTO atualizarDoador(DoadorDTO doadorDTO, Integer doadorId) {
-        return null;
+    public DoadorDTO atualizarDoador(DoadorRequestDTO doadorRequestDTO, Integer doadorId) {
+        Doador doador = _doadorRepository.findById(doadorId).orElseThrow();
+        EnderecoDTO enderecoDTO = _enderecoService.buscarEnderecoComDoadorId(doadorId);
+
+        Optional.ofNullable(doadorRequestDTO.getNome()).filter(nome -> !nome.isEmpty()).ifPresent(doador::setNome);
+        Optional.ofNullable(doadorRequestDTO.getEmail()).filter(email -> !email.isEmpty()).ifPresent(doador::setEmail);
+        Optional.ofNullable(doadorRequestDTO.getAniversario()).ifPresent(doador::setAniversario);
+        Optional.ofNullable(doadorRequestDTO.getPeso()).ifPresent(doador::setPeso);
+        Optional.ofNullable(doadorRequestDTO.getGenero()).filter(genero -> !genero.isEmpty()).ifPresent(doador::setGenero);
+        Optional.ofNullable(doadorRequestDTO.getTipoSanguineo()).filter(tipoSanguineo -> !tipoSanguineo.isEmpty()).ifPresent(doador::setTipoSanguineo);
+        Optional.ofNullable(doadorRequestDTO.getEnderecoDTO().getBairro()).filter(bairro -> !bairro.isEmpty()).ifPresent(enderecoDTO::setBairro);
+        Optional.ofNullable(doadorRequestDTO.getEnderecoDTO().getCidade()).filter(cidade -> !cidade.isEmpty()).ifPresent(enderecoDTO::setCidade);
+        Optional.ofNullable(doadorRequestDTO.getEnderecoDTO().getEstado()).filter(estado -> !estado.isEmpty()).ifPresent(enderecoDTO::setEstado);
+        Optional.ofNullable(doadorRequestDTO.getEnderecoDTO().getCep()).filter(cep -> !cep.isEmpty()).ifPresent(enderecoDTO::setCep);
+
+        _doadorRepository.save(doador);
+        _enderecoService.atualizarEndereco(enderecoDTO, doadorId);
+
+        return _modelMapper.map(doador, DoadorDTO.class);
     }
 
     @Override
