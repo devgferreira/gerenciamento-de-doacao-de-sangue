1568ad33418149874617982a187fd838435bb3aa
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-15 13:59:39 -0300

    feat: criando o tratamento para exception: handlerDoadorNaoEncontradoException - RestExceptionHandler

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/infra/exceptions/handler/RestExceptionHandler.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/infra/exceptions/handler/RestExceptionHandler.java
@@ -11,5 +11,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-
+    @ExceptionHandler(DoadorNaoEncontradoException.class)
+    public final ResponseEntity<Object> handlerDoadorNaoEncontradoException(DoadorNaoEncontradoException ex){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(ErrorCodes.DOADOR_NAO_ENCONTRADO, ex.getMessage());
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
+    }
 }
