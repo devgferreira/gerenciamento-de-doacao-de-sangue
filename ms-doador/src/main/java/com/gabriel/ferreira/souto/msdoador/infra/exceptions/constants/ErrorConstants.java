edb333e2dbfd84cf86b72ddcf0373db270fdf84f
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-15 12:23:42 -0300

    feat: criando a constant: ErrorConstants

---
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/infra/exceptions/constants/ErrorConstants.java
@@ -0,0 +1,10 @@
+package com.gabriel.ferreira.souto.msdoador.infra.exceptions.constants;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Getter(AccessLevel.PRIVATE)
+public class ErrorConstants {
+    public static final String DOADOR_NAO_ENCONTRADO = " Ms-Doador - Doador não encontrado.";
+    public static final String ENDERECO_NAO_ENCONTRADO = " Ms-Doador - Endereço não encontrado.";
+}
