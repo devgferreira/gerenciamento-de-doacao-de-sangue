6173cb1ded42f6f7fc3c957a931ff8087bab37b4
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-15 14:02:56 -0300

    feat: criando o tratamento para exception: handlerEnderecoNaoEncontradoException - RestExceptionHandler

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/infra/exceptions/handler/RestExceptionHandler.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/infra/exceptions/handler/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.gabriel.ferreira.souto.msdoador.infra.exceptions.handler;
 
 import com.gabriel.ferreira.souto.msdoador.domain.enums.ErrorCodes;
 import com.gabriel.ferreira.souto.msdoador.infra.exceptions.DoadorNaoEncontradoException;
+import com.gabriel.ferreira.souto.msdoador.infra.exceptions.EnderecoNaoEncontradoException;
 import com.gabriel.ferreira.souto.msdoador.infra.exceptions.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,12 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DoadorNaoEncontradoException.class)
     public final ResponseEntity<Object> handlerDoadorNaoEncontradoException(DoadorNaoEncontradoException ex){
         ExceptionResponse exceptionResponse = new ExceptionResponse(ErrorCodes.DOADOR_NAO_ENCONTRADO, ex.getMessage());
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(EnderecoNaoEncontradoException.class)
+    public final ResponseEntity<Object> handlerEnderecoNaoEncontradoException(EnderecoNaoEncontradoException ex){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(ErrorCodes.ENDERECO_NAO_ENCONTRADO, ex.getMessage());
         return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
     }
 }
