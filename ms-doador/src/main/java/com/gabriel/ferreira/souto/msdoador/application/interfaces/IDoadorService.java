b754d7a9ac7f6611a552fa146ba4e06e90eae726
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-15 11:15:26 -0300

    feat: mudando o Response dos métodos: criarDoador e atualizarDoador - IDoadorService

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IDoadorService.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/application/interfaces/IDoadorService.java
@@ -5,8 +5,8 @@ import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorRequestDTO;
 import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorResponseDTO;
 
 public interface IDoadorService {
-    DoadorDTO criarDoador(DoadorRequestDTO doadorRequestDTO);
-    DoadorDTO atualizarDoador(DoadorRequestDTO doadorRequestDTO, Integer doadorId);
+    DoadorResponseDTO criarDoador(DoadorRequestDTO doadorRequestDTO);
+    DoadorResponseDTO atualizarDoador(DoadorRequestDTO doadorRequestDTO, Integer doadorId);
     DoadorResponseDTO buscarDoadorComId(Integer doadorId);
     void deletarDoadorComId(Integer doadorId);
 }
