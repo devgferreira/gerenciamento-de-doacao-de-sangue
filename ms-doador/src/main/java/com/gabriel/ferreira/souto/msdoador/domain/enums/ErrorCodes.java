1f9dec8a5b300689e539d8e10e2b7e9a6793c06d
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-16 17:30:13 -0300

    feat: criando o error code: TIPO_SANGUINEO_INVALIDO - ErrorCodes

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/enums/ErrorCodes.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/enums/ErrorCodes.java
@@ -10,6 +10,7 @@ public enum ErrorCodes {
     ENDERECO_NAO_ENCONTRADO("Endereço não foi encontrado"),
     EMAIL_JA_EXISTE("Email já existe."),
     ENDERECO_INVALIDO("Endereço inváilido, por favor, preencha todos os campos."),
+    TIPO_SANGUINEO_INVALIDO("Tipo sanguineo inválido, por favor, coloque um tipo válido."),
     EMAIL_NAO_ENCONTRADO("Email não encontrado.");
     private final String mensagem;
 
