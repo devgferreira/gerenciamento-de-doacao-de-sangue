e899cb16b43ceef6030310e20e1d73c00448fb3e
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-16 14:21:22 -0300

    feat: criando o handler: handlerEnderecoInvalidoException - RestExceptionHandler

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/infra/exceptions/handler/RestExceptionHandler.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/infra/exceptions/handler/RestExceptionHandler.java
@@ -31,4 +31,9 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         ExceptionResponse exceptionResponse = new ExceptionResponse(ErrorCodes.EMAIL_JA_EXISTE, ex.getMessage());
         return  ResponseEntity.status(HttpStatus.OK).body(exceptionResponse);
     }
+    @ExceptionHandler(EnderecoInvalidoException.class)
+    public final ResponseEntity<Object> handlerEnderecoInvalidoException(EnderecoInvalidoException ex){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(ErrorCodes.ENDERECO_INVALIDO, ex.getMessage());
+        return  ResponseEntity.status(HttpStatus.OK).body(exceptionResponse);
+    }
 }
