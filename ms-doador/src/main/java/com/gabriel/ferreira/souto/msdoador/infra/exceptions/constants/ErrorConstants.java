f17a31cd578a0d049baa03f1a0a475196713bb0b
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-03-16 18:01:24 -0300

    feat: criando a constant: DOADOR_INVALIDO - ErrorConstants

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/infra/exceptions/constants/ErrorConstants.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/infra/exceptions/constants/ErrorConstants.java
@@ -6,9 +6,10 @@ import lombok.Getter;
 @Getter(AccessLevel.PRIVATE)
 public class ErrorConstants {
     public static final String DOADOR_NAO_ENCONTRADO = " Ms-Doador - Doador não encontrado.";
-    public static final String ENDERECO_NAO_ENCONTRADO = " Ms-Doador - Endereço não encontrado.";
+    public static final String DOADOR_INVALIDO = " Ms-Doador - Doador inválido.";
+    public static final String TIPO_SANGUINEO_INVALIDO = " Ms-Doador - Tipo sanguineo inválido.";
     public static final String EMAIL_JA_EXISTE = " Ms-Doador - Email já existe.";
     public static final String EMAIL_NAO_ENCONTRADO = " Ms-Doador - Email não encontrado.";
+    public static final String ENDERECO_NAO_ENCONTRADO = " Ms-Doador - Endereço não encontrado.";
     public static final String ENDERECO_INVALIDO = " Ms-Doador - Endereço Inválido.";
-    public static final String TIPO_SANGUINEO_INVALIDO = " Ms-Doador - Tipo sanguineo inválido.";
 }
