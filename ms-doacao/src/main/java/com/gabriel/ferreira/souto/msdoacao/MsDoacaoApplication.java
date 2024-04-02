bfada187798ff503fecd6a4832d57786d5aefe9f
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-02 20:01:13 -0300

    feat: subindo a estrutura inicial do ms-doacao

---
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/MsDoacaoApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.souto.msdoacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDoacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDoacaoApplication.class, args);
+	}
+
+}
