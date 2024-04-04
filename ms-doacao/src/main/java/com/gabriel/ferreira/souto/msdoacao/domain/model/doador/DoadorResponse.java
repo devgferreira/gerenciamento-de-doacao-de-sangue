dab70c5542c00e4b74bfb12780d8604855a6b78a
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-04 17:22:12 -0300

    criando o objeto DoadorResponse

---
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/model/doador/DoadorResponse.java
@@ -0,0 +1,12 @@
+package com.gabriel.ferreira.souto.msdoacao.domain.model.doador;
+
+import java.util.Date;
+
+public class DoadorResponse {
+    private String nome;
+    private String email;
+    private Date aniversario;
+    private String genero;
+    private Integer peso;
+    private String tipoSanguineo;
+}
