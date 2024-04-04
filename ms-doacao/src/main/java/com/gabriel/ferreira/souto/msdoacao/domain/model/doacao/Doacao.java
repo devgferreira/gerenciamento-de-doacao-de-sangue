4a775cb087b0a1a7e459873bf3c82a83ec9aa655
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-04 17:29:45 -0300

    feat: criando a entidade Doacao

---
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/model/doacao/Doacao.java
@@ -0,0 +1,21 @@
+package com.gabriel.ferreira.souto.msdoacao.domain.model.doacao;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Doacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer doadorId;
+    private Integer sangueML;
+}
