2c9702e6ef5f16337a04cd27991ff1cce3d3999d
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-04 17:30:05 -0300

    feat: criando a interface do repository: IDoacaoRepository

---
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/interfaces/IDoacaoRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.ferreira.souto.msdoacao.domain.interfaces;
+
+import com.gabriel.ferreira.souto.msdoacao.domain.model.doacao.Doacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IDoacaoRepository extends JpaRepository<Doacao, Integer> {
+}
