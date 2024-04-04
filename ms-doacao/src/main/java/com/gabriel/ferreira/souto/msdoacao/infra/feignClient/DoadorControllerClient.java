8666f0fe36e648d10807edfcaba2990c35080444
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-04 17:22:59 -0300

    feat: adicioando o end-point: buscarDoadorComId - DoadorControllerClient

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/feignClient/DoadorControllerClient.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/feignClient/DoadorControllerClient.java
@@ -1,8 +1,13 @@
 package com.gabriel.ferreira.souto.msdoacao.infra.feignClient;
 
+import com.gabriel.ferreira.souto.msdoacao.domain.model.doador.DoadorResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(value = "ms-doador", path = "/doadores")
 public interface DoadorControllerClient {
-
+    @GetMapping(value = "/{doadorId}")
+    ResponseEntity<DoadorResponse> buscarDoadorComId(@PathVariable Integer doadorId);
 }
