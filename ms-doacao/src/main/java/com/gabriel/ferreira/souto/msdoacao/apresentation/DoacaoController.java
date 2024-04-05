774e586295bdd9d244fc64be43fe2ad3a8e83692
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-05 12:20:07 -0300

    feat: criando o construtor para a interface IDoacaoService - DoacaoController

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/apresentation/DoacaoController.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/apresentation/DoacaoController.java
@@ -1,4 +1,12 @@
 package com.gabriel.ferreira.souto.msdoacao.apresentation;
 
+import com.gabriel.ferreira.souto.msdoacao.application.interfaces.IDoacaoService;
+import com.gabriel.ferreira.souto.msdoacao.domain.interfaces.IDoacaoRepository;
+
 public class DoacaoController {
+    private final IDoacaoService _doacaoService;
+
+    public DoacaoController(IDoacaoService doacaoService) {
+        _doacaoService = doacaoService;
+    }
 }
