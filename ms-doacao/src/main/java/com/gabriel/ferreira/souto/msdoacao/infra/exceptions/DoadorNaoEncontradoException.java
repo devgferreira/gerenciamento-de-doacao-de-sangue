cd97841a13139010c3321b8f2d9df53666181447
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-05 11:48:24 -0300

    feat: criando a exception: DoadorNaoEncontradoException

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/DoadorNaoEncontradoException.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/DoadorNaoEncontradoException.java
@@ -1,13 +1,13 @@
-package com.gabriel.ferreira.souto.msdoador.infra.exceptions;
+package com.gabriel.ferreira.souto.msdoacao.infra.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.OK)
-public class EnderecoInvalidoException extends RuntimeException{
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DoadorNaoEncontradoException {
     private ExceptionResponse exceptionResponse;
 
-    public EnderecoInvalidoException(ExceptionResponse exceptionResponse) {
+    public DoadorNaoEncontradoException(ExceptionResponse exceptionResponse) {
         super();
         this.exceptionResponse = exceptionResponse;
     }
