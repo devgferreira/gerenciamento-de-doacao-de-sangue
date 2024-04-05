5d472e6f4cef1ebb22c0d42d45e084c6fe1a5b9d
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-05 11:50:57 -0300

    fix: adicionando o extends RuntimeException

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/DoadorNaoEncontradoException.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/DoadorNaoEncontradoException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class DoadorNaoEncontradoException {
+public class DoadorNaoEncontradoException extends RuntimeException {
     private ExceptionResponse exceptionResponse;
 
     public DoadorNaoEncontradoException(ExceptionResponse exceptionResponse) {
