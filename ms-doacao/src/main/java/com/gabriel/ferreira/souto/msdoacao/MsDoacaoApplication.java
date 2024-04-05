5efcbaf261856e4b3128f87af157ee6d060f0665
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-05 12:15:38 -0300

    feat: adicionando a anotação @EnableDiscoveryClient

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/MsDoacaoApplication.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/MsDoacaoApplication.java
@@ -2,10 +2,13 @@ package com.gabriel.ferreira.souto.msdoacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class MsDoacaoApplication {
 
 	public static void main(String[] args) {
--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/MsDoadorApplication.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/MsDoadorApplication.java
@@ -2,8 +2,10 @@ package com.gabriel.ferreira.souto.msdoador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsDoadorApplication {
 
     public static void main(String[] args) {
