69c7248beb55dbea03a96cc7def3b831e3738d77
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-05 10:27:01 -0300

    feat: criando o construtores - DoacaoService

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/application/service/DoacaoService.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/application/service/DoacaoService.java
@@ -1,7 +1,19 @@
 package com.gabriel.ferreira.souto.msdoacao.domain.application.service;
 
+import com.gabriel.ferreira.souto.msdoacao.domain.interfaces.IDoacaoRepository;
+import com.gabriel.ferreira.souto.msdoacao.infra.feignClient.DoadorControllerClient;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DoacaoService {
+    private final ModelMapper _modelMapper;
+    private final IDoacaoRepository _doacaoRepository;
+    private final DoadorControllerClient _doadorControllerClient;
+
+    public DoacaoService(ModelMapper modelMapper, IDoacaoRepository doacaoRepository, DoadorControllerClient doadorControllerClient) {
+        _modelMapper = modelMapper;
+        _doacaoRepository = doacaoRepository;
+        _doadorControllerClient = doadorControllerClient;
+    }
 }
