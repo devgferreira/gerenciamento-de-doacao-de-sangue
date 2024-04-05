7437fdc354a5ee5693eaa248aa1b59da974ba074
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-05 10:35:46 -0300

    feat: implementando o método: criarDoacao() - DoacaoService

---
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/application/service/DoacaoService.java
@@ -0,0 +1,40 @@
+package com.gabriel.ferreira.souto.msdoacao.application.service;
+
+import com.gabriel.ferreira.souto.msdoacao.application.dtos.DoacaoDTO;
+import com.gabriel.ferreira.souto.msdoacao.application.interfaces.IDoacaoService;
+import com.gabriel.ferreira.souto.msdoacao.domain.interfaces.IDoacaoRepository;
+import com.gabriel.ferreira.souto.msdoacao.domain.model.doacao.Doacao;
+import com.gabriel.ferreira.souto.msdoacao.infra.feignClient.DoadorControllerClient;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DoacaoService implements IDoacaoService {
+    private final ModelMapper _modelMapper;
+    private final IDoacaoRepository _doacaoRepository;
+    private final DoadorControllerClient _doadorControllerClient;
+
+    public DoacaoService(ModelMapper modelMapper, IDoacaoRepository doacaoRepository, DoadorControllerClient doadorControllerClient) {
+        _modelMapper = modelMapper;
+        _doacaoRepository = doacaoRepository;
+        _doadorControllerClient = doadorControllerClient;
+    }
+
+    @Override
+    public DoacaoDTO criarDoacao(DoacaoDTO doacaoDTO) {
+        Doacao doacao = _modelMapper.map(doacaoDTO, Doacao.class);
+        return _modelMapper.map(_doacaoRepository.save(doacao), DoacaoDTO.class);
+    }
+
+    @Override
+    public List<DoacaoDTO> listarTodasAsDoacao() {
+        return null;
+    }
+
+    @Override
+    public DoacaoDTO buscarDoacaoPorId(Integer doacaoId) {
+        return null;
+    }
+}
