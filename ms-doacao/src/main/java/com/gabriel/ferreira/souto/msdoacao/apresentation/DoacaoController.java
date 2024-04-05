1fe5ec77c84bd16418e597d24973bd16c8fd8792
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-05 12:22:29 -0300

    feat: Criando o end-point: criarDoacao - DoacaoController

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/apresentation/DoacaoController.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/apresentation/DoacaoController.java
@@ -1,7 +1,11 @@
 package com.gabriel.ferreira.souto.msdoacao.apresentation;
 
+import com.gabriel.ferreira.souto.msdoacao.application.dtos.DoacaoDTO;
 import com.gabriel.ferreira.souto.msdoacao.application.interfaces.IDoacaoService;
-import com.gabriel.ferreira.souto.msdoacao.domain.interfaces.IDoacaoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class DoacaoController {
     private final IDoacaoService _doacaoService;
@@ -9,4 +13,11 @@ public class DoacaoController {
     public DoacaoController(IDoacaoService doacaoService) {
         _doacaoService = doacaoService;
     }
+
+    @PostMapping
+    public ResponseEntity<DoacaoDTO> criarDoacao(@RequestBody DoacaoDTO doacaoDTO){
+        DoacaoDTO doacaoDTOResult = _doacaoService.criarDoacao(doacaoDTO);
+        return new ResponseEntity<>(doacaoDTOResult, HttpStatus.CREATED);
+    }
+
 }
