58988938272601f50992edb5029efd558636441e
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-05 12:16:47 -0300

    fix: mundando o path da anotação @FeignClient - DoadorControllerClient

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/feignClient/DoadorControllerClient.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/feignClient/DoadorControllerClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "ms-doador", path = "/doadores")
+@FeignClient(value = "ms-doador", path = "/api/doadores")
 public interface DoadorControllerClient {
     @GetMapping(value = "/{doadorId}")
     ResponseEntity<DoadorResponse> buscarDoadorComId(@PathVariable Integer doadorId);
