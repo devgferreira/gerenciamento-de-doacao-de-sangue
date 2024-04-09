cf9fb177e3ede2aad2d3df3d3238e3117d92096a
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-09 12:06:24 -0300

    feat: criando a constants: IDADE_INVALIDA - ErrorConstants

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/constants/ErrorConstants.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/constants/ErrorConstants.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 
 @Getter(AccessLevel.PRIVATE)
 public class ErrorConstants {
-    public static final String DOADOR_NAO_ENCONTRADO = "Ms-Doacao - Doador não encontrado";
-    public static final String DOACAO_NAO_ENCONTRADO = "Ms-Doacao - Doação não encontrado";
-    public static final String PESO_INVALIDO = "Ms-Doacao - Peso inválido, minímo de 50kg para realizar a doação";
+    public static final String DOADOR_NAO_ENCONTRADO = "Ms-Doacao - Doador não encontrado.";
+    public static final String DOACAO_NAO_ENCONTRADO = "Ms-Doacao - Doação não encontrado.";
+    public static final String IDADE_INVALIDA = "Ms-Doacao - Idade inválida, idade miníma para doação é de 18 anos.";
+    public static final String PESO_INVALIDO = "Ms-Doacao - Peso inválido, minímo de 50kg para realizar a doação.";
 }
