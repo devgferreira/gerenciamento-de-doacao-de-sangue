32bd02b2ca99312ee6b0f59ff692c467a8f158f9
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-09 16:10:21 -0300

    feat: criando o método no repository: findFirstByDoadorIdOrderByIdDesc - IDoacaoRepository

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/interfaces/IDoacaoRepository.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/interfaces/IDoacaoRepository.java
@@ -4,6 +4,9 @@ import com.gabriel.ferreira.souto.msdoacao.domain.model.doacao.Doacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface IDoacaoRepository extends JpaRepository<Doacao, Integer> {
+    Doacao findFirstByDoadorIdOrderByIdDesc(Integer doadorId);
 }
