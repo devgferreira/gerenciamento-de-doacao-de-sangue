246d429ed77ca19ab8575b2e7a7a402d3896f68d
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-09 11:41:38 -0300

    feat: adicionando a anotação @Data - DoadorResponse

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/model/doador/DoadorResponse.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/model/doador/DoadorResponse.java
@@ -1,7 +1,10 @@
 package com.gabriel.ferreira.souto.msdoacao.domain.model.doador;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class DoadorResponse {
     private String nome;
     private String email;
