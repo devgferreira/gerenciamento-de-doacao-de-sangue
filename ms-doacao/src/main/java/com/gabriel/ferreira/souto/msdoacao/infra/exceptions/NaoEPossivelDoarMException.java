0c147ef08c1c7603ed70a08c2712b6c39cb53090
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-10 12:33:15 -0300

    fix: mudando o nome da exception - NaoEPossivelDoarMException

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/NaoEPossivelDoarMException.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/NaoEPossivelDoarMException.java
@@ -4,10 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.OK)
-public class NaoEPossivelDoarException extends RuntimeException {
+public class NaoEPossivelDoarMException extends RuntimeException {
     private ExceptionResponse exceptionResponse;
 
-    public NaoEPossivelDoarException(ExceptionResponse exceptionResponse) {
+    public NaoEPossivelDoarMException(ExceptionResponse exceptionResponse) {
         super();
         this.exceptionResponse = exceptionResponse;
     }
