ab0e8252cdebeb8fe0e69c5f6f849a91baf295fa
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-10 17:23:52 -0300

    feat: subindo o ms-cloud-geteway

---
+++ ms-cloud-geteway/src/test/java/com/gabriel/ferreira/souto/mscloudgeteway/MsCloudGetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.souto.mscloudgeteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudGetewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
