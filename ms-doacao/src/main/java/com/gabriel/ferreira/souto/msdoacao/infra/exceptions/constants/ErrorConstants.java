5cdc72331b3796bdc7bd699df8f12e370ef794eb
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-10 12:44:52 -0300

    fix: mudando o nome da constants NAO_E_POSIVEL_DOAR para NAO_E_POSIVEL_DOAR_M - ErrorConstants

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/constants/ErrorConstants.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/constants/ErrorConstants.java
@@ -8,5 +8,5 @@ public class ErrorConstants {
     public static final String DOACAO_NAO_ENCONTRADO = "Ms-Doacao - Doação não encontrado.";
     public static final String IDADE_INVALIDA = "Ms-Doacao - Idade inválida, idade miníma para doação é de 18 anos.";
     public static final String PESO_INVALIDO = "Ms-Doacao - Peso inválido, minímo de 50kg para realizar a doação.";
-    public static final String NAO_E_POSIVEL_DOAR = "Ms-Doacao - Não é possivel doar, o tempo entre as doação não foram atingidos.";
+    public static final String NAO_E_POSIVEL_DOAR_M = "Ms-Doacao - A Doação tem um tempo de espera de 90 em 90 dias para homens.";
 }
