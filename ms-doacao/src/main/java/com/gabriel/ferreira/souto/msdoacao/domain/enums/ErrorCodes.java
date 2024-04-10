6da89da5db9964ba68f743bf7792d2ed75830929
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-10 12:30:44 -0300

    feat: criando o error code: NAO_E_POSSIVEL_DOAR_M e NAO_E_POSSIVEL_DOAR_F - ErrorCodes

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/enums/ErrorCodes.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/enums/ErrorCodes.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 public enum ErrorCodes {
     DOACAO_NAO_ENCONTRADA("Doação não encontrada."),
     IDADE_INVALIDA ("Idade inválida, idade miníma para doação é de 18 anos."),
+    NAO_E_POSSIVEL_DOAR_M ("A Doação tem um tempo de espera de 90 em 90 dias para homens."),
+    NAO_E_POSSIVEL_DOAR_F ("A Doação tem um tempo de espera de 60 em 60 dias para mulheres."),
     PESO_INVALIDO ("Peso inválido, minímo de 50kg para realizar a doação.");
     private final String mensagem;
 }
