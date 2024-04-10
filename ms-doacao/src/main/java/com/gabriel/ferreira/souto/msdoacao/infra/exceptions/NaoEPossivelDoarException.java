99ad55432f6f5eae665889714385e4e23fca32a9
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-10 12:32:09 -0300

    feat: criando a exception: NaoEPossivelDoarException

---
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/infra/exceptions/NaoEPossivelDoarException.java
@@ -0,0 +1,14 @@
+package com.gabriel.ferreira.souto.msdoacao.infra.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.OK)
+public class NaoEPossivelDoarException extends RuntimeException {
+    private ExceptionResponse exceptionResponse;
+
+    public NaoEPossivelDoarException(ExceptionResponse exceptionResponse) {
+        super();
+        this.exceptionResponse = exceptionResponse;
+    }
+}
