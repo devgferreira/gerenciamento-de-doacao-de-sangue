04bfdd3fcc260eaec46bcb8471ef124a1dd28ee2
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-10 12:53:50 -0300

    feat: criando o error code: QUANTIDADE_DE_SANGUE_INVALIDA - ErrorCodes

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/enums/ErrorCodes.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/domain/enums/ErrorCodes.java
@@ -10,6 +10,7 @@ public enum ErrorCodes {
     IDADE_INVALIDA ("Idade inválida, idade miníma para doação é de 18 anos."),
     NAO_E_POSSIVEL_DOAR_M ("A Doação tem um tempo de espera de 90 em 90 dias para homens."),
     NAO_E_POSSIVEL_DOAR_F ("A Doação tem um tempo de espera de 60 em 60 dias para mulheres."),
+    QUANTIDADE_DE_SANGUE_INVALIDA ("Quantidade de mililitros de sangue doados deve ser entre 420ml e 470ml."),
     PESO_INVALIDO ("Peso inválido, minímo de 50kg para realizar a doação.");
     private final String mensagem;
 }
