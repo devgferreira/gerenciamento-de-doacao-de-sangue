6c3edd4a75056f19e9208a757107458e520da9f1
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-10 22:05:10 -0300

    feat: criando a busca: findByCpf - IDoadorRepository

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IDoadorRepository.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IDoadorRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface IDoadorRepository extends JpaRepository<Doador, Integer> {
     Optional<Doador> findByEmail(String email);
+    Optional<Doador> findByCpf(String cpf);
 }
