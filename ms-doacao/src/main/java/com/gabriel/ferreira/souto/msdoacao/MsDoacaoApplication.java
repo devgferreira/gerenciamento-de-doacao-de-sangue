98f1783d56e708852b8b11c7455cd84e44ed3ecf
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-15 18:07:22 -0300

    feat: adicionando a anotação @EnableRabbit - MsDoacaoApplication

--- ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/MsDoacaoApplication.java
+++ ms-doacao/src/main/java/com/gabriel/ferreira/souto/msdoacao/MsDoacaoApplication.java
@@ -1,5 +1,6 @@
 package com.gabriel.ferreira.souto.msdoacao;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableRabbit
 public class MsDoacaoApplication {
 
 	public static void main(String[] args) {
