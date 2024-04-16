9812f0150ef4fd04afdbc138968c4ecbf7134a1f
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-16 16:50:22 -0300

    feaet: implementando a interface: IEstoqueService - EstoqueService

--- ms-estoque/src/main/java/com/gabriel/ferreira/souto/msestoque/application/service/EstoqueService.java
+++ ms-estoque/src/main/java/com/gabriel/ferreira/souto/msestoque/application/service/EstoqueService.java
@@ -1,16 +1,30 @@
 package com.gabriel.ferreira.souto.msestoque.application.service;
 
+import com.gabriel.ferreira.souto.msestoque.application.dto.EstoqueDTO;
+import com.gabriel.ferreira.souto.msestoque.application.interfaces.IEstoqueService;
 import com.gabriel.ferreira.souto.msestoque.domain.repository.IEstoqueRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class EstoqueService {
+public class EstoqueService implements IEstoqueService {
     private final IEstoqueRepository _estoqueRepository;
     private final ModelMapper _modelMapper;
 
     public EstoqueService(IEstoqueRepository estoqueRepository, ModelMapper modelMapper) {
         _estoqueRepository = estoqueRepository;
         _modelMapper = modelMapper;
+    }
+
+    @Override
+    public List<EstoqueDTO> bsucarTodasOsEstoques() {
+        return null;
+    }
+
+    @Override
+    public EstoqueDTO buscarEstoquePorId(Integer id) {
+        return null;
     }
 }
