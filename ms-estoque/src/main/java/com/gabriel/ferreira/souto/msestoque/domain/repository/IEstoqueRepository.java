28a738f733df4af6937de282da360329e29d2fb9
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-16 16:52:24 -0300

    fix: mudando o tipo de objeto do JpaRepository - IEstoqueRepository

--- ms-estoque/src/main/java/com/gabriel/ferreira/souto/msestoque/domain/repository/IEstoqueRepository.java
+++ ms-estoque/src/main/java/com/gabriel/ferreira/souto/msestoque/domain/repository/IEstoqueRepository.java
@@ -3,5 +3,5 @@ package com.gabriel.ferreira.souto.msestoque.domain.repository;
 import com.gabriel.ferreira.souto.msestoque.domain.model.estoque.Estoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IEstoqueRepository extends JpaRepository<Integer, Estoque> {
+public interface IEstoqueRepository extends JpaRepository<Estoque, Integer > {
 }
