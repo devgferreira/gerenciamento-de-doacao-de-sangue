8d18a76325fb2ae19d373fc2bde61ddb4a51875a
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-17 12:27:34 -0300

    feat: criando o end-point: bsucarTodosOsEstoques - EstoqueController

--- ms-estoque/src/main/java/com/gabriel/ferreira/souto/msestoque/apresentation/EstoqueController.java
+++ ms-estoque/src/main/java/com/gabriel/ferreira/souto/msestoque/apresentation/EstoqueController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/estoques")
 public class EstoqueController {
@@ -21,5 +23,10 @@ public class EstoqueController {
     public ResponseEntity<EstoqueDTO> buscarEstoquePorId(@PathVariable Integer estoqueId){
         EstoqueDTO estoqueDTO = _estoqueService.buscarEstoquePorId(estoqueId);
         return new ResponseEntity<>(estoqueDTO, HttpStatus.OK);
+    }
+    @GetMapping()
+    public ResponseEntity<List<EstoqueDTO>> bsucarTodosOsEstoques(){
+        List<EstoqueDTO> estoquesDTO = _estoqueService.bsucarTodasOsEstoques();
+        return new ResponseEntity<>(estoquesDTO, HttpStatus.OK);
     }
 }
