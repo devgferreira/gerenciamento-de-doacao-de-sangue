d86b9157d4ddf4004f6a9bb954d94cfe741c5a6c
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-17 12:52:41 -0300

    feat: colocando a anotação @Enumerated para transformar o enum: Genero em String - Doador

--- ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/model/doador/Doador.java
+++ ms-doador/src/main/java/com/gabriel/ferreira/souto/msdoador/domain/model/doador/Doador.java
@@ -1,10 +1,7 @@
 package com.gabriel.ferreira.souto.msdoador.domain.model.doador;
 
 import com.gabriel.ferreira.souto.msdoador.domain.enums.Genero;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,6 +20,7 @@ public class Doador {
     private String nome;
     private String email;
     private Date aniversario;
+    @Enumerated(value = EnumType.STRING)
     private Genero genero;
     private Integer peso;
     private String tipoSanguineo;
