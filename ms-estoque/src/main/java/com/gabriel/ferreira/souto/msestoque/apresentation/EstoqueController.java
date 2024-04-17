fb0bc55ef9ca3b3658ae858e92487a60b94b56e8
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-04-17 12:26:17 -0300

    feat: criando o end-point: buscarEstoquePorId - EstoqueController

--- ms-estoque/src/main/java/com/gabriel/ferreira/souto/msestoque/apresentation/EstoqueController.java
+++ ms-estoque/src/main/java/com/gabriel/ferreira/souto/msestoque/apresentation/EstoqueController.java
@@ -1,6 +1,11 @@
 package com.gabriel.ferreira.souto.msestoque.apresentation;
 
+import com.gabriel.ferreira.souto.msestoque.application.dto.EstoqueDTO;
 import com.gabriel.ferreira.souto.msestoque.application.interfaces.IEstoqueService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,9 @@ public class EstoqueController {
     public EstoqueController(IEstoqueService estoqueService) {
         _estoqueService = estoqueService;
     }
-
+    @GetMapping(value = "/{estoqueId}")
+    public ResponseEntity<EstoqueDTO> buscarEstoquePorId(@PathVariable Integer estoqueId){
+        EstoqueDTO estoqueDTO = _estoqueService.buscarEstoquePorId(estoqueId);
+        return new ResponseEntity<>(estoqueDTO, HttpStatus.OK);
+    }
 }
