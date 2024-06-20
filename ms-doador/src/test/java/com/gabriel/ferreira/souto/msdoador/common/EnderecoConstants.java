5f29ce26aaa07b0645886c1115102d979f12b2b6
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-20 16:23:10 -0300

    feat: criando a constant: ENDERECO_DTO_VALIDO - EnderecoConstants

--- ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/common/EnderecoConstants.java
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/common/EnderecoConstants.java
@@ -1,4 +1,7 @@
 package com.gabriel.ferreira.souto.msdoador.common;
 
+import com.gabriel.ferreira.souto.msdoador.application.dtos.EnderecoDTO;
+
 public class EnderecoConstants {
+    public static final EnderecoDTO ENDERECO_DTO_VALIDO = new EnderecoDTO("Vila Maria", "SP", "SP", "02128020");
 }
