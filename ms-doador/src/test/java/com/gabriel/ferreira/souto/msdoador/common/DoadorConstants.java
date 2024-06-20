6e4f56b9042d84559bd87f58c69da8c54a653f58
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-20 16:39:50 -0300

    test: criando as constants: DOADOR_REQUEST_DTO_INVALIDO e DOADOR_REQUEST_DTO_INVALIDO - DoadorConstants

--- ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/common/DoadorConstants.java
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/common/DoadorConstants.java
@@ -9,12 +9,28 @@ import com.gabriel.ferreira.souto.msdoador.domain.model.doador.Doador;
 import java.util.Date;
 
 public class DoadorConstants {
-    public static final Doador DOADOR_VALIDO = new Doador("82116296072", "Gabriel", "gabrie@gmail.com",
+    public static final Doador DOADOR_VALIDO =
+            new Doador("82116296072", "Gabriel", "gabrie@gmail.com",
             new Date(), Genero.M, 60, "B-");
-    public static final DoadorRequestDTO DOADOR_REQUEST_DTO_VALIDO = new DoadorRequestDTO("82116296072", "Gabriel", "gabrie@gmail.com",
-            new Date(), Genero.M, 60, "B-",new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
-    public static final DoadorResponseDTO DOADOR_RESPONSE_DTO_VALIDO = new DoadorResponseDTO("82116296072", "Gabriel", "gabrie@gmail.com",
-            new Date(), Genero.M, 60, "B-",new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
+
+    public static final DoadorRequestDTO DOADOR_REQUEST_DTO_VALIDO =
+            new DoadorRequestDTO("82116296072", "Gabriel", "gabrie@gmail.com",
+            new Date(), Genero.M, 60, "B-",
+            new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
+    public static final DoadorRequestDTO DOADOR_REQUEST_DTO_COM_CPF_INVALIDO =
+            new DoadorRequestDTO("821162960721", "Gabriel", "gabrie@gmail.com",
+                    new Date(), Genero.M, 60, "B-",
+                    new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
+
+    public static final DoadorRequestDTO DOADOR_REQUEST_DTO_INVALIDO =
+            new DoadorRequestDTO("", "", "",
+            null, null,null , "",
+            new EnderecoDTO("", "", "", ""));
+
+    public static final DoadorResponseDTO DOADOR_RESPONSE_DTO_VALIDO =
+            new DoadorResponseDTO("82116296072", "Gabriel", "gabrie@gmail.com",
+            new Date(), Genero.M, 60, "B-",
+            new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
 
 
 }
