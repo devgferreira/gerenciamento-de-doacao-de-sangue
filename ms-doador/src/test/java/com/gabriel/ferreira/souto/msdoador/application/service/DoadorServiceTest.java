16dec8348a9e2ffc86c325b6ce9cbe0e9d8ff2d3
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-20 16:23:43 -0300

    feat: criando o teste: testCriarDoador_QuandoDoadorValidoSalvo_RetornandoDoador - DoadorServiceTest

--- ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorServiceTest.java
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/application/service/DoadorServiceTest.java
@@ -1,7 +1,53 @@
 package com.gabriel.ferreira.souto.msdoador.application.service;
 
+import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorRequestDTO;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorResponseDTO;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.EnderecoDTO;
+import com.gabriel.ferreira.souto.msdoador.application.interfaces.IDoadorService;
+import com.gabriel.ferreira.souto.msdoador.application.interfaces.IEnderecoService;
+import com.gabriel.ferreira.souto.msdoador.domain.enums.Genero;
+import static com.gabriel.ferreira.souto.msdoador.common.DoadorConstants.*;
+import static com.gabriel.ferreira.souto.msdoador.common.EnderecoConstants.*;
+import com.gabriel.ferreira.souto.msdoador.domain.interfaces.IDoadorRepository;
+import com.gabriel.ferreira.souto.msdoador.domain.model.doador.Doador;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.Date;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+public class DoadorServiceTest {
+    @Mock
+    private IDoadorRepository _doadorRepository;
+    @Mock
+    private IEnderecoService _enderecoService;
+    @Mock
+    private ModelMapper _modelMapper;
+    @InjectMocks
+    private DoadorService _doadorService;
+    @Test
+    void testCriarDoador_QuandoDoadorValidoSalvo_RetornandoDoador(){
 
-class DoadorServiceTest {
+        when(_doadorRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        when(_doadorRepository.findByCpf(anyString())).thenReturn(Optional.empty());
+        when(_modelMapper.map(DOADOR_REQUEST_DTO_VALIDO, Doador.class)).thenReturn(DOADOR_VALIDO);
+        when(_doadorRepository.save(DOADOR_VALIDO)).thenReturn(DOADOR_VALIDO);
+        when(_enderecoService.criarEndereco(DOADOR_REQUEST_DTO_VALIDO.getEndereco(),
+                DOADOR_REQUEST_DTO_VALIDO.getCpf())).thenReturn(ENDERECO_DTO_VALIDO);
+        when(_modelMapper.map(DOADOR_REQUEST_DTO_VALIDO, DoadorResponseDTO.class)).thenReturn(DOADOR_RESPONSE_DTO_VALIDO);
 
+
+
+        DoadorResponseDTO result = _doadorService.criarDoador(DOADOR_REQUEST_DTO_VALIDO);
+
+        assertNotNull(result);
+    }
 }
