1b62bf73df6502d1d7ff6b3e522b40a02e211c2d
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-20 13:11:54 -0300

    test: criando o test: testDoador_QuandoSalvar_RetornandoDoadorSalvo - IDoadorRepositoryTest

--- ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IDoadorRepositoryTest.java
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IDoadorRepositoryTest.java
@@ -1,7 +1,24 @@
 package com.gabriel.ferreira.souto.msdoador.domain.interfaces;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.gabriel.ferreira.souto.msdoador.domain.model.doador.Doador;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static com.gabriel.ferreira.souto.msdoador.common.DoadorConstants.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
 class IDoadorRepositoryTest {
 
+    @Autowired
+    private IDoadorRepository _doadorRepository;
+
+    @Test
+    void testDoador_QuandoSalvar_RetornandoDoadorSalvo(){
+        Doador result = _doadorRepository.save(DOADOR_VALIDO);
+
+        assertNotNull(result);
+        assertTrue(result.getId() > 0);
+        assertEquals(DOADOR_VALIDO, result);
+    }
 }
