2c93e1de3e8f7d5d72b43aebab9933f6a6b22be2
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-20 16:20:40 -0300

    feat: criando as constants: DOADOR_REQUEST_DTO_VALIDO e DOADOR_RESPONSE_DTO_VALIDO - DoadorConstants

--- ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/common/DoadorConstants.java
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/common/DoadorConstants.java
@@ -1,5 +1,8 @@
 package com.gabriel.ferreira.souto.msdoador.common;
 
+import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorRequestDTO;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorResponseDTO;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.EnderecoDTO;
 import com.gabriel.ferreira.souto.msdoador.domain.enums.Genero;
 import com.gabriel.ferreira.souto.msdoador.domain.model.doador.Doador;
 
@@ -8,4 +11,10 @@ import java.util.Date;
 public class DoadorConstants {
     public static final Doador DOADOR_VALIDO = new Doador("82116296072", "Gabriel", "gabrie@gmail.com",
             new Date(), Genero.M, 60, "B-");
+    public static final DoadorRequestDTO DOADOR_REQUEST_DTO_VALIDO = new DoadorRequestDTO("82116296072", "Gabriel", "gabrie@gmail.com",
+            new Date(), Genero.M, 60, "B-",new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
+    public static final DoadorResponseDTO DOADOR_RESPONSE_DTO_VALIDO = new DoadorResponseDTO("82116296072", "Gabriel", "gabrie@gmail.com",
+            new Date(), Genero.M, 60, "B-",new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
+
+
 }
