701a35bcd42d317772e69e351dcda15906d13062
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-24 12:09:14 -0300

    test: criando o test: testEndereco_QuandoUpdate_RetorandoEnderecoAtualizado - IEnderecoRepositoryTest

--- ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IEnderecoRepositoryTest.java
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IEnderecoRepositoryTest.java
@@ -21,12 +21,31 @@ class IEnderecoRepositoryTest {
         assertEquals(ENDERECO_VALIDO, result);
     }
     @Test
-    void testEndereco_QuandoBuscarEnderecoPorId_RetorandoEnderec(){
+    void testEndereco_QuandoBuscarEnderecoPorId_RetorandoEndereco(){
         _enderecoRepository.save(ENDERECO_VALIDO);
         Endereco result = _enderecoRepository.findByDoadorCpf(ENDERECO_VALIDO.getDoadorCpf()).get();
 
         assertNotNull(result);
         assertTrue(result.getId() > 0);
         assertEquals(ENDERECO_VALIDO.getDoadorCpf(), result.getDoadorCpf());
+    }
+    @Test
+    void testEndereco_QuandoUpdate_RetorandoEnderecoAtualizado(){
+        _enderecoRepository.save(ENDERECO_VALIDO);
+        Endereco enderecoSavalvado = _enderecoRepository.findByDoadorCpf(ENDERECO_VALIDO.getDoadorCpf()).get();
+        enderecoSavalvado.setBairro("XYB");
+        enderecoSavalvado.setEstado("LSC");
+        enderecoSavalvado.setCidade("PQE");
+        enderecoSavalvado.setCep("1231245");
+        enderecoSavalvado.setDoadorCpf("12312457891011");
+        Endereco result = _enderecoRepository.save(enderecoSavalvado);
+
+        assertNotNull(result);
+        assertTrue(result.getId() > 0);
+        assertEquals("XYB", result.getBairro());
+        assertEquals("LSC", result.getEstado());
+        assertEquals("PQE", result.getCidade());
+        assertEquals("1231245", result.getCep());
+        assertEquals("12312457891011", result.getDoadorCpf());
     }
 }
