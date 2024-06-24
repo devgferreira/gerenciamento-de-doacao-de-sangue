c117e42ea018e82595146686a2b87055c7dacf07
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-24 13:52:16 -0300

    test: criando o test: testcriarEndereco_ComEnderecoValido_RetornadoEndereco - EnderecoServiceTest

--- ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/application/service/EnderecoServiceTest.java
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/application/service/EnderecoServiceTest.java
@@ -1,7 +1,42 @@
 package com.gabriel.ferreira.souto.msdoador.application.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.EnderecoDTO;
+import com.gabriel.ferreira.souto.msdoador.domain.interfaces.IEnderecoRepository;
+import com.gabriel.ferreira.souto.msdoador.domain.model.endereco.Endereco;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import static com.gabriel.ferreira.souto.msdoador.common.EnderecoConstants.*;
 
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
 class EnderecoServiceTest {
+
+    @InjectMocks
+    private EnderecoService _enderecoService;
+    @Mock
+    private IEnderecoRepository _enderecoRepository;
+    @Mock
+    private ModelMapper _modelMapper;
+
+    @Test
+    void testcriarEndereco_ComEnderecoValido_RetornadoEndereco(){
+        ENDERECO_VALIDO.setDoadorCpf("123");
+
+        when(_modelMapper.map(ENDERECO_DTO_VALIDO, Endereco.class)).thenReturn(ENDERECO_VALIDO);
+        when(_enderecoRepository.save(ENDERECO_VALIDO)).thenReturn(ENDERECO_VALIDO);
+        when(_modelMapper.map(ENDERECO_VALIDO, EnderecoDTO.class)).thenReturn(ENDERECO_DTO_VALIDO);
+
+        EnderecoDTO result = _enderecoService.criarEndereco(ENDERECO_DTO_VALIDO, ENDERECO_VALIDO.getDoadorCpf());
+
+        assertNotNull(result);
+        assertEquals(ENDERECO_DTO_VALIDO, result);
+
+    }
 
 }
