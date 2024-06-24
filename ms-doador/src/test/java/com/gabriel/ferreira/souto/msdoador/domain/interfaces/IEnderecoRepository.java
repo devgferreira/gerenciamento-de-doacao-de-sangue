953ed756c2bda06154d27a14a40bf1eeeb4718b7
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-24 11:52:33 -0300

    test: criiando a classe de test: IEnderecoRepository

---
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/domain/interfaces/IEnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.gabriel.ferreira.souto.msdoador.domain.interfaces;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class IEnderecoRepository {
+}
