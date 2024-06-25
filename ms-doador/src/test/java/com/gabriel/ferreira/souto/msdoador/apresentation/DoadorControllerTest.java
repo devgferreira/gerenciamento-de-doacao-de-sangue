4c72f8f440679ba2e4700a4bae991941a88f7285
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-25 10:37:18 -0300

    test: criando o test: testCriarDoador_QuandoDoadorValido_RetornadoDoadorCriado - DoadorControllerTest

--- ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/apresentation/DoadorControllerTest.java
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/apresentation/DoadorControllerTest.java
@@ -1,15 +1,27 @@
 package com.gabriel.ferreira.souto.msdoador.apresentation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gabriel.ferreira.souto.msdoador.application.dtos.DoadorRequestDTO;
 import com.gabriel.ferreira.souto.msdoador.application.interfaces.IDoadorService;
 import com.gabriel.ferreira.souto.msdoador.application.service.DoadorService;
+import com.gabriel.ferreira.souto.msdoador.domain.model.doador.Doador;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Date;
+
+import static com.gabriel.ferreira.souto.msdoador.common.DoadorConstants.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 class DoadorControllerTest {
@@ -19,4 +31,13 @@ class DoadorControllerTest {
     private ObjectMapper _objectMapper;
     @MockBean
     private IDoadorService _doadorService;
+
+    @SneakyThrows
+    @Test
+    void testCriarDoador_QuandoDoadorValido_RetornadoDoadorCriado(){
+        when(_doadorService.criarDoador(any(DoadorRequestDTO.class))).thenReturn(DOADOR_RESPONSE_DTO_VALIDO);
+        _mockMvc.perform(post("/api/doadores").content(_objectMapper.writeValueAsString(DOADOR_REQUEST_DTO_VALIDO)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated()).andExpect(jsonPath("$.cpf").value(DOADOR_RESPONSE_DTO_VALIDO.getCpf()));
+    }
+
 }
--- ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/common/DoadorConstants.java
+++ ms-doador/src/test/java/com/gabriel/ferreira/souto/msdoador/common/DoadorConstants.java
@@ -6,34 +6,35 @@ import com.gabriel.ferreira.souto.msdoador.application.dtos.EnderecoDTO;
 import com.gabriel.ferreira.souto.msdoador.domain.enums.Genero;
 import com.gabriel.ferreira.souto.msdoador.domain.model.doador.Doador;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class DoadorConstants {
     public static final Doador DOADOR_VALIDO =
             new Doador("82116296072", "Gabriel", "gabrie@gmail.com",
-            new Date(), Genero.M, 60, "B-");
+            new Date(11, 4, 11, 18, 0), Genero.M, 60, "B-");
     public static final Doador DOADOR_VALIDO_2 =
             new Doador("82116296072", "Lucas", "lucas@gmail.com",
-                    new Date(), Genero.M, 80, "A+-");
+                    new Date(11, 4, 11, 18, 0), Genero.M, 80, "A+-");
 
     public static final DoadorRequestDTO DOADOR_REQUEST_DTO_VALIDO =
             new DoadorRequestDTO("82116296072", "Gabriel", "gabrie@gmail.com",
-            new Date(), Genero.M, 60, "B-",
+            new Date(11, 4, 11, 18, 0), Genero.M, 60, "B-",
             new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
 
     public static final DoadorRequestDTO DOADOR_REQUEST_DTO_COM_ENDERECO_INVALIDO =
             new DoadorRequestDTO("82116296072", "Gabriel", "gabrie@gmail.com",
-            new Date(), Genero.M, 60, "B-",
+            new Date(11, 4, 11, 18, 0), Genero.M, 60, "B-",
             new EnderecoDTO("", "", "", ""));
 
     public static final DoadorRequestDTO DOADOR_REQUEST_DTO_COM_CPF_INVALIDO =
             new DoadorRequestDTO("8211629607", "Gabriel", "gabrie@gmail.com",
-            new Date(), Genero.M, 60, "B-",
+            new Date(11, 4, 11, 18, 0), Genero.M, 60, "B-",
             new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
 
     public static final DoadorRequestDTO DOADOR_REQUEST_DTO_COM_TIPO_SANGUINEO_INVALIDO =
             new DoadorRequestDTO("82116296072", "Gabriel", "gabrie@gmail.com",
-            new Date(), Genero.M, 60, "C-",
+            new Date(11, 4, 11, 18, 0), Genero.M, 60, "C-",
             new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
 
     public static final DoadorRequestDTO DOADOR_REQUEST_DTO_INVALIDO =
@@ -43,7 +44,7 @@ public class DoadorConstants {
 
     public static final DoadorResponseDTO DOADOR_RESPONSE_DTO_VALIDO =
             new DoadorResponseDTO("82116296072", "Gabriel", "gabrie@gmail.com",
-            new Date(), Genero.M, 60, "B-",
+            new Date(11, Calendar.MAY, 11, 18, 0), Genero.M, 60, "B-",
             new EnderecoDTO("Vila Maria", "SP", "SP", "02128020"));
 
 
